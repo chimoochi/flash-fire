670c15723e371e7141c855a21a99be23d8c451bf
Timothy  Nguyen <timothysvn@gmail.com>
2026-01-30 15:49:34 -0800

    placeholder health functions

--- GodotGame/player/health_bar.gd
+++ GodotGame/player/health_bar.gd
@@ -1,5 +1,14 @@
 extends ProgressBar
-
+var health = 100
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pass
+
+func set_health(amount:int):
+	pass
+	
+func add_health(amount:int):
+	pass
+	
+func remove_health(amount:int):
 	pass
