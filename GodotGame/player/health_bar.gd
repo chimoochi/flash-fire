c803c090c945f9f50c63b6c57a60e9eaa704e216
garpyz <gali5@s.sfusd.edu>
2026-02-06 15:50:29 -0800

    healthbar drain

--- GodotGame/player/health_bar.gd
+++ GodotGame/player/health_bar.gd
@@ -1,13 +1,24 @@
 extends ProgressBar
 
+var health_tween: Tween
+
 func _ready() -> void:
 	pass
 
 func set_health(amount: int):
-	value = amount
+	_animate_value(amount)
 	
 func add_health(amount: int):
-	value = min(max_value, value + amount)
+	var target = min(max_value, value + amount)
+	_animate_value(target)
 	
 func remove_health(amount: int):
-	value = max(min_value, value - amount)
+	var target = max(min_value, value - amount)
+	_animate_value(target)
+
+func _animate_value(target_value: float) -> void:
+	if health_tween:
+		health_tween.kill()
+	
+	health_tween = create_tween()
+	health_tween.tween_property(self, "value", target_value, 0.4).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
